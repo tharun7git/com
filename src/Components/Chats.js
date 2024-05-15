0a16bc96dfd337204b25d3d04a20216fc4fa94ee
tharun7git <116146974+tharun7git@users.noreply.github.com>
2024-05-15 12:23:00 -0700

    Chats.js

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,9 +1,40 @@
-// Filename - component/Chats.js
+// Filename - App.js
 
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Chats from "./Components/Chats"; // Corrected import to match the new component name
+import About from "./Components/about";
+import Contact from "./Components/contact";
+import "./App.css";
 
-function Chats() {
-  return <h1>Welcome to the world of Geeks!</h1>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Chats />}></Route> {/* Updated Route */}
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+          <footer className="App-footer">
+            {/* New footer section */}
+            <ul className="App-header">
+              <li>
+                <Link to="/">Chats</Link>
+              </li>
+              <li>
+                <Link to="/about">Updates</Link>
+              </li>
+              <li>
+                <Link to="/contact">Calls</Link>
+              </li>
+            </ul>
+          </footer>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default Chats;
+export default App;
